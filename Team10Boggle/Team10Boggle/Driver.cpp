cf441501ba42fdcc07ddbe7dd5a8e20720028ea7
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-04-14 16:25:44 -0400

    Removed Misc code from Driver. Moved PlayerManager to model folder.

--- Team10Boggle/Team10Boggle/Driver.cpp
+++ Team10Boggle/Team10Boggle/Driver.cpp
@@ -11,17 +11,5 @@ int main(array<System::String ^> ^args)
 
 	Application::Run(gcnew BoggleForm());
 
-	String^ delimStr = " ,.:\t";
-	Console::WriteLine("delimiter : '{0}'", delimStr);
-	array<Char>^ delimiter = delimStr->ToCharArray();
-	array<String^>^ words;
-	String^ line = "one\ttwo three:four,five six seven";
-
-	Console::WriteLine("text : '{0}'", line);
-	words = line->Split(delimiter);
-	Console::WriteLine("Number of Words : {0}", words->Length);
-	for (int word = 0; word<words->Length; word++)
-		Console::WriteLine("{0}", words[word]);
-
 	return 0;
 }
