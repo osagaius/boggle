97c3bd4cf3cb469d5c5870a1d18b05515ca6ed76
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-04-16 01:43:09 -0400

    Added BoggleSolver class.

---
+++ Team10Boggle/Team10Boggle/BoggleSolver.h
@@ -0,0 +1,36 @@
+#pragma once
+#pragma once
+#include <windows.h>
+#using <System.dll>
+#using <System.Drawing.dll>
+#using <System.Windows.Forms.dll>
+#include "Trie.h"
+#include "Word.h"
+using namespace model;
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Windows::Forms;
+using namespace System::Text;
+using namespace System::ComponentModel;
+
+namespace model
+{
+	ref class BoggleSolver
+	{
+	public:
+		BoggleSolver();
+		void solveBoard(array<String^, 2>^ board, array<boolean, 2>^ tracker, Trie^ lexicon, String^ word, int x, int y, List<String^>^ result);
+
+		property List<String^>^ Words
+		{
+			List<String^>^ get(){ return this->words; };
+			void set(List<String^>^ listOfWords){ this->words = listOfWords; };
+		}
+
+	private:
+		List<String^>^ words;
+		Trie^ dictionary;
+		bool isDefinedWord(String^ word);
+	};
+
+}
